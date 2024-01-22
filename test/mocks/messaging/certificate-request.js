ef3551a8ebedba308c9947fdd57f0c0f496653fe
Shaun Fitzsimons <shaun.r.fitzsimons@gmail.com>
2024-01-22 12:16:48 +0000

    fix message schema (#3)
    * Fix certificate message schema
    
    ---------
    
    Co-authored-by: steve-dickinson <steve.dickinson@defra.gov.uk>

--- test/mocks/messaging/certificate-request.js
+++ test/mocks/messaging/certificate-request.js
@@ -1,55 +1,47 @@
 const valid = {
   body: {
-    data: {
-      exemptionOrder: 2015,
-      certificateId: '24bfda00-efa5-41d4-8711-9d31d7fcca65',
-      owner: {
-        name: 'Mr Owner',
-        address: {
-          line1: '1 The Street',
-          postcode: 'AB12 3CD'
-        }
-      },
-      dog: {
-        indexNumber: 'ED1234',
-        microchipNumber: '12345',
-        name: 'Fido',
-        breed: 'Breed 1',
-        sex: 'Male',
-        birthDate: new Date('2019-01-01'),
-        colour: 'White',
-        certificateIssued: new Date('2020-01-01')
+    exemptionOrder: 2015,
+    certificateId: '24bfda00-efa5-41d4-8711-9d31d7fcca65',
+    owner: {
+      name: 'Mr Owner',
+      address: {
+        line1: '1 The Street',
+        postcode: 'AB12 3CD'
       }
     },
-    type: 'uk.gov.defra.aphw.ddi.certificate.requested',
-    source: 'aphw-ddi-portal'
+    dog: {
+      indexNumber: 'ED1234',
+      microchipNumber: '12345',
+      name: 'Fido',
+      breed: 'Breed 1',
+      sex: 'Male',
+      birthDate: new Date('2019-01-01'),
+      colour: 'White',
+      certificateIssued: new Date('2020-01-01')
+    }
   }
 }
 
 const invalid = {
   body: {
-    data: {
-      exemptionOrder: 2027,
-      certificateId: '24bfda00-efa5-41d4-8711-9d31d7fcca65',
-      owner: {
-        name: 'Mr Owner',
-        address: {
-          line1: '1 The Street',
-          postcode: 'AB12 3CD'
-        }
-      },
-      dog: {
-        indexNumber: 'ED1234',
-        microchipNumber: '12345',
-        name: 'Fido',
-        breed: 'Breed 1',
-        sex: 'Male',
-        birthDate: new Date('2019-01-01'),
-        colour: 'White',
-        certificateIssued: new Date('2020-01-01')
+    exemptionOrder: 2027,
+    owner: {
+      name: 'Mr Owner',
+      address: {
+        line1: '1 The Street',
+        postcode: 'AB12 3CD'
       }
     },
-    source: 'aphw-ddi-portal'
+    dog: {
+      indexNumber: 'ED1234',
+      microchipNumber: '12345',
+      name: 'Fido',
+      breed: 'Breed 1',
+      sex: 'Male',
+      birthDate: new Date('2019-01-01'),
+      colour: 'White',
+      certificateIssued: new Date('2020-01-01')
+    }
   }
 }
 
